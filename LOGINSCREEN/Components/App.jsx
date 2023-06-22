7af11a3b29be02aa2c63b496045c477fd39ce262
ShaistaDaniya <shaista@gigandtake.com>
2023-06-22 13:51:28 +0530

    11

--- LOGINSCREEN/Components/App.jsx
+++ LOGINSCREEN/Components/App.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Screen1 from './screen1';
-import Screen2 from './screen2';
-import Screen3 from './screen3';
-import Screen4 from './screen4';
+import Screen1 from './screen1.jsx';
+import Screen2 from './screen2.jsx';
+import Screen3 from './screen3.jsx';
+import Screen4 from './screen4.jsx';
+import Screen4 from './screen5.jsx';
+import Screen4 from './screen6.jsx';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -39,6 +42,20 @@ const App = () => {
           component={Screen4}
           options={{
             title: 'Screen 4',
+          }}
+        />
+        <Stack.Screen
+          name="Screen5"
+          component={Screen4}
+          options={{
+            title: 'Screen 5',
+          }}
+        />
+        <Stack.Screen
+          name="Screen6"
+          component={Screen4}
+          options={{
+            title: 'Screen 6',
           }}
         />
       </Stack.Navigator>
